6a931dfb7efbd886d653bc4f763ac4ef80183b2a
beardie <iliailemkov@gmail.com>
2018-07-24 16:52:53 +0300

    В дата классах предпочтительнее имутабельные переменные

--- app/src/main/java/com/example/beardie/currencyholder/data/model/ExchangeCurrency.kt
+++ app/src/main/java/com/example/beardie/currencyholder/data/model/ExchangeCurrency.kt
@@ -3,8 +3,8 @@ package com.example.beardie.currencyholder.data.model
 import java.util.*
 
 data class ExchangeCurrency (
-    var id: String = UUID.randomUUID().toString(),
-    var fromCurrency: FinanceCurrency,
-    var toCurrency: FinanceCurrency,
-    var coef: Double
+    val id: String = UUID.randomUUID().toString(),
+    val fromCurrency: FinanceCurrency,
+    val toCurrency: FinanceCurrency,
+    val coef: Double
 )
--- app/src/main/java/com/example/beardie/currencyholder/data/model/FinanceCurrency.kt
+++ app/src/main/java/com/example/beardie/currencyholder/data/model/FinanceCurrency.kt
@@ -3,9 +3,9 @@ package com.example.beardie.currencyholder.data.model
 import java.util.*
 
 data class FinanceCurrency (
-    var id: String = UUID.randomUUID().toString(),
-    var symbol: String,
-    var title: String,
-    var shortTitle: String,
-    var imageUri: String
+    val id: String = UUID.randomUUID().toString(),
+    val symbol: String,
+    val title: String,
+    val shortTitle: String,
+    val imageUri: String
 )
